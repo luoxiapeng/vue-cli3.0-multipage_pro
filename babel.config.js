2473972c5acedf9da9998876f48c8c398e8a4395
luoxiapeng <949123073@qq.com>
2019-04-10 13:28:20 +0800

    更新

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,5 @@
-// 去掉 console.log
-const plugins = [];
-if(['production', 'prod'].includes(process.env.NODE_ENV)) {
-  plugins.push("transform-remove-console")
-}
 module.exports = {
   presets: [
     '@vue/app'
-  ],
-  plugins: plugins
+  ]
 }
--- src/pages/page2/main.js
+++ src/pages/page2/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './page2.vue'
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
